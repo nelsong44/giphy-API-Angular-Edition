7b876d2d9429d1a4b0397a68f626a292e24a6c92
nelsong44 <nelsong44@gmail.com>
2017-06-14 14:31:39 -0500

    retrieving data from giphy api matching user keyword search

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,16 +3,21 @@ console.log('JS linked');
 var giphyApp = angular.module('giphyApp', []);
 
 giphyApp.controller('GiphyController', function($http) { //$http bullt-in- have to pass in as parameter to use
-  console.log('NG linked');
-  var vm = this; //references our GiphyController
-  // vm.baseFunction = function() {
-  //   console.log('in baseFunction');
-  //   $http({
-  //     method: 'GET',
-  //     url: '/giphy'
-  //   }).then(function(response) {
-  //     console.log('back with', response); // the giphy array as an object
-  //   }); //end get
+    console.log('NG linked');
+    var vm = this; //references our GiphyController
+    vm.gifArray = [];
+    vm.searchKeyword = function() {
+      console.log('called searchKeyword()');
+      $http({
+        method: 'GET',
+        url: 'http://api.giphy.com/v1/gifs/search?q=' +  vm.input + '&api_key=dc6zaTOxFJmzC'
+      }).then(function(response) {
+        console.log('back with', response); //gif as object-array of 25 objects matching keyword search //response.data[i].url
+        vm.gifArray.push(response.data[i].url);
+      }); //end get
+    //empty input field after button click
+    searchKeyword = '';
+  }; // end searchKeyword
 
   vm.randomGiphy = function() {
     console.log('randomGiphy');
--- routes/giphy.js
+++ routes/giphy.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 //------------------------------------------
-var gifsArray = [];
+
+var gifArray = [];
+
+// vm.searchKeyword =
+vm.searchUrl = 'http://api.giphy.com/v1/gifs/search?q=' +   + '&api_key=dc6zaTOxFJmzC';
+
 
 module.exports = router;
